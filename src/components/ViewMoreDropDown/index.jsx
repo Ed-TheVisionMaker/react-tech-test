dc67292ef3d7e1f3a875c1bd0491399cc02174ba
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-09 18:03:39 +0000

    styling of dropdown structure

--- src/components/ViewMoreDropDown/index.jsx
+++ src/components/ViewMoreDropDown/index.jsx
@@ -12,25 +12,27 @@ function ViewMoreDropDown() {
     setShowDropDown((prevState) => !prevState);
   };
   return (
-    <div>
-      <div>{option}</div>
-      <button onClick={handleShowDropDown}>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='home-chevronDown'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='m19.5 8.25-7.5 7.5-7.5-7.5'
-          />
-        </svg>
-      </button>
-      <div>
+    <div className='viewMore_Container'>
+      <div className='viewMore_displayWrapper'>
+        <div>{option}</div>
+        <button onClick={handleShowDropDown}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='home-chevronDown'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='m19.5 8.25-7.5 7.5-7.5-7.5'
+            />
+          </svg>
+        </button>
+      </div>
+      <div className='viewMore_optionsWrapper'>
         <button id='button1' onClick={handleOptionChange}>
           10
         </button>
