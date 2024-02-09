1d8102b55fe30f04508a76d1f9a1948cfbb87825
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-09 14:55:42 +0000

    required drink data extracted. Test value displayed in UI

--- src/pages/Drink/index.jsx
+++ src/pages/Drink/index.jsx
@@ -1,29 +1,53 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
 
+{
+  /*The detail screen should display the
+drink's image,
+name of the drink, 
+alcohol by volume (abv),
+tagline,
+full description,
+and one other piece of data of your choice. */
+}
+
 function Drink() {
+  const [drinkData, setDrinkData] = useState({});
   const params = useParams();
   const id = params.drinkId;
 
-  console.log(id, "id")
-
   const fetchDrinkData = async (id) => {
     try {
-      const { data } = await axios.get(`https://api.punkapi.com/v2/beers/${id}`);
-      console.log(data, "data")
+      const { data } = await axios.get(
+        `https://api.punkapi.com/v2/beers/${id}`
+      );
+      const drinkData = extractRequiredData(data);
+      console.log(drinkData, 'drinkData');
+      setDrinkData(drinkData);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const extractRequiredData = (data) => {
+    const { name, image_url, abv, tagline, description, food_pairing } =
+      data[0];
+    return {
+      name,
+      imageUrl: image_url,
+      abv,
+      tagline,
+      description,
+      foodPairing: food_pairing,
+    };
+  };
+
   useEffect(() => {
     fetchDrinkData(id);
-  });
+  }, []);
 
+  return <div>{drinkData.abv}</div>;
+}
 
-    return <div>Drink</div>;
-  };
-  
-  export default Drink;
-  
+export default Drink;
