f0364e73588ab8f0f519d8a025eb431528d278c6
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-09 09:41:47 +0000

    fetched drink data success

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function Home() {
-  return <div>Home</div>;
-};
+  const [drinkList, setDrinkList] = useState([]);
+
+  const fetchDrinkData = async () => {
+    try {
+      const { data } = await axios.get('https://api.punkapi.com/v2/beers');
+      const drinkList = createDrinkList(data);
+      setDrinkList(drinkList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const createDrinkList = (data) => {
+    const drinksRequired = data.slice(0, 10);
+    return drinksRequired.map((drink) => {
+      return {
+        id: drink.id,
+        image: drink.image_url,
+        name: drink.name,
+        description: drink.description,
+      };
+    });
+  };
+
+  useEffect(() => {
+    console.log('useEffect called');
+    fetchDrinkData();
+  }, []);
+
+  return <div>
+    <ul>
+        {drinkList.map((drink) => (<li key={drink.id}>{drink.name}</li>))}
+    </ul>
+  </div>;
+}
 
 export default Home;
