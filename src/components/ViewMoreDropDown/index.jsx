ba256d674e219d8a517797c21d1b09c803ce0252
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-09 19:53:19 +0000

    retrieve and display drinks based on user selection

--- src/components/ViewMoreDropDown/index.jsx
+++ src/components/ViewMoreDropDown/index.jsx
@@ -1,28 +1,18 @@
-import { useState } from 'react';
+function ViewMoreDropDown(props) {
+  const {numberOfDrinks, handleNumberChange, handleShowDropDown, showDropDown} = props;
 
-function ViewMoreDropDown() {
-  const [option, setOption] = useState(10);
-  const [showDropDown, setShowDropDown] = useState(false);
-
-  const handleOptionChange = (e) => {
-    setOption(e.target.innerText);
-  };
-
-  const handleShowDropDown = () => {
-    setShowDropDown((prevState) => !prevState);
-  };
   return (
-    <div className='viewMore_Container'>
-      <div className='viewMore_displayWrapper'>
-        <div>{option}</div>
-        <button onClick={handleShowDropDown}>
+    <div className='view-more-container'>
+      <div className='view-more-display-wrapper'>
+        <div>{numberOfDrinks}</div>
+        <button onClick={() => handleShowDropDown()}>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
             viewBox='0 0 24 24'
             strokeWidth={1.5}
             stroke='currentColor'
-            className='home-chevronDown'
+            className='view-more-chevron-down'
           >
             <path
               strokeLinecap='round'
@@ -32,17 +22,19 @@ function ViewMoreDropDown() {
           </svg>
         </button>
       </div>
-      <div className='viewMore_optionsWrapper'>
-        <button id='button1' onClick={handleOptionChange}>
-          10
-        </button>
-        <button id='button2' onClick={(e) => handleOptionChange(e)}>
-          20
-        </button>
-        <button id='button3' onClick={(e) => handleOptionChange(e)}>
-          30
-        </button>
-      </div>
+      {showDropDown && (
+        <div className='view-more-options-wrapper'>
+          <button id='button1' onClick={(e) => handleNumberChange(e)}>
+            10
+          </button>
+          <button id='button2' onClick={(e) => handleNumberChange(e)}>
+            20
+          </button>
+          <button id='button3' onClick={(e) => handleNumberChange(e)}>
+            30
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,21 +7,67 @@ import ViewMoreDropDown from '../../components/ViewMoreDropDown';
 import { Link } from 'react-router-dom';
 
 function Home() {
-  const [drinkList, setDrinkList] = useState([]);
+  const [drinksList, setDrinksList] = useState([]);
+  const [numberOfDrinks, setNumberOfDrinks] = useState(10);
+  const [showDropDown, setShowDropDown] = useState(false);
 
-  const fetchDrinkData = async () => {
+  const handleShowDropDown = () => {
+    setShowDropDown((prevState) => !prevState);
+  };
+
+  const handleNumberChange = (e) => {
+    const parseDrinksNumber = parseInt(e.target.innerText);
+    setNumberOfDrinks(parseDrinksNumber);
+    setShowDropDown((prevState) => !prevState);
+  };
+
+
+  const fetchDefaultData = async () => {
     try {
       const { data } = await axios.get('https://api.punkapi.com/v2/beers');
-      const drinkList = createDrinkList(data);
-      setDrinkList(drinkList);
+      return data;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const createDrinkList = (data) => {
-    const drinksRequired = data.slice(0, 10);
-    return drinksRequired.map((drink) => {
+  const fetchLargeData = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://api.punkapi.com/v2/beers?per_page=30'
+      );
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchRequiredDrinks = async (numberOfDrinks) => {
+    let data = [];
+    if (numberOfDrinks === 10 || numberOfDrinks === 20) {
+      data = await fetchDefaultData();
+    }
+    if (numberOfDrinks === 30) {
+      data = await fetchLargeData();
+    }
+    createDrinksList(data);
+  };
+
+  const trimDrinkData = (data) => {
+    let trimmedData = [];
+    if (numberOfDrinks === 10) {
+      trimmedData = data.slice(0, 10);
+    } else if (numberOfDrinks === 20) {
+      trimmedData = data.slice(0, 20);
+    } else if (numberOfDrinks === 30) {
+      trimmedData = data;
+    }
+    return trimmedData;
+  };
+
+  const createDrinksList = (data) => {
+    const drinksRequired = trimDrinkData(data);
+    const drinksList = drinksRequired.map((drink) => {
       return {
         id: drink.id,
         imageUrl: drink.image_url,
@@ -29,22 +75,28 @@ function Home() {
         description: drink.description,
       };
     });
+    setDrinksList(drinksList);
   };
 
   useEffect(() => {
-    fetchDrinkData();
-  }, []);
+    fetchRequiredDrinks(numberOfDrinks);
+  }, [numberOfDrinks]);
 
   return (
     <div className={'home_Container'}>
       <h2 className={'home_Title'}>Punk Drinks</h2>
-      <ViewMoreDropDown />
+      <ViewMoreDropDown
+        numberOfDrinks={numberOfDrinks}
+        handleNumberChange={handleNumberChange}
+        handleShowDropDown={handleShowDropDown}
+        showDropDown={showDropDown}
+      />
       <ul className={'home_drinksContainer'}>
-        {drinkList.map((drink) => (
+        {drinksList.map((drink) => (
           <Link className='link' key={drink.id} to={`drink/${drink.id}`}>
             <li className='home_drinkListItem'>
               <div className={'home_drinkDisplayWrapper'}>
-                <DisplayDrink drink={drink}  location={'Home'} />
+                <DisplayDrink drink={drink} location={'Home'} />
                 <DisplayText drink={drink} location={'Home'} />
               </div>
             </li>
