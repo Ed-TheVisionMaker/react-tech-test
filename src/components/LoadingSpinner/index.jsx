967952d49454c9e307832008253573f8e620da39
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-10 16:02:00 +0000

    altered position of loading spinner

--- src/components/LoadingSpinner/index.jsx
+++ src/components/LoadingSpinner/index.jsx
@@ -1,12 +1,11 @@
-import "./LoadingSpinner.css"
+import './LoadingSpinner.css';
 
 function LoadingSpinner() {
   return (
-    <div className="spinner-container">
-      <div className="loading-spinner">
-      </div>
+    <div className='spinner-container'>
+      <div className='loading-spinner'></div>
     </div>
   );
-};
+}
 
 export default LoadingSpinner;
