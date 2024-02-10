2bc27a4b5b748462312164ad9007bac968800bc0
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-10 16:12:11 +0000

    code  formatted on all pages

--- src/components/DrinkImage/index.jsx
+++ src/components/DrinkImage/index.jsx
@@ -1,7 +1,11 @@
 function DrinkImage({ drinkData, location }) {
   const { name, imageUrl } = drinkData;
   return (
-      <img className={location === 'Home' ? 'home-drink-image' : 'drink-drink-image'} src={imageUrl} alt={name} />
+    <img
+      className={location === 'Home' ? 'home-drink-image' : 'drink-drink-image'}
+      src={imageUrl}
+      alt={name}
+    />
   );
 }
 
--- src/components/DrinkText/index.jsx
+++ src/components/DrinkText/index.jsx
@@ -1,45 +1,47 @@
 function DrinkText({ drinkData, location }) {
   const { name, description, tagline, abv, foodPairing } = drinkData;
 
- const textHomePage = () => {
-  return (
-    <div className='home-text-wrapper'>
-      <h2 className='home-drink-name'>{name}</h2>
-      <p className='home-drink-description'>{description}</p>
-    </div>
-  )
- }
+  const textHomePage = () => {
+    return (
+      <div className='home-text-wrapper'>
+        <h2 className='home-drink-name'>{name}</h2>
+        <p className='home-drink-description'>{description}</p>
+      </div>
+    );
+  };
 
- const foodPairingList = () => {
-  return (
-    <div className='drink-food-pairing-list'>
-      <h3>Food Pairing</h3>
-      <ul>
-        {foodPairing?.map((food) => (
-          <li className={'drink-food-pairing-item'} key={food.id}>{food.description}</li>
-        ))}
-      </ul>
-    </div>
-  )
- }
+  const foodPairingList = () => {
+    return (
+      <div className='drink-food-pairing-list'>
+        <h3>Food Pairing</h3>
+        <ul>
+          {foodPairing?.map((food) => (
+            <li className={'drink-food-pairing-item'} key={food.id}>
+              {food.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
- const textDrinkPage = () => {
-    const taglinePeriodRemoved = tagline?.replace('.', '')
-  return (
-    <div className='drink-text-wrapper'>
-      <h2 className='drink-drink-name'>{name}</h2>
-      <p className='drink-tagline'>{taglinePeriodRemoved}</p>
-      <p className='drink-abv'>{`${abv}%`}</p>
-      <p className='drink-description'>{description}</p>
-      {foodPairingList()}
-    </div>
-  )
- }
+  const textDrinkPage = () => {
+    const taglinePeriodRemoved = tagline?.replace('.', '');
+    return (
+      <div className='drink-text-wrapper'>
+        <h2 className='drink-drink-name'>{name}</h2>
+        <p className='drink-tagline'>{taglinePeriodRemoved}</p>
+        <p className='drink-abv'>{`${abv}%`}</p>
+        <p className='drink-description'>{description}</p>
+        {foodPairingList()}
+      </div>
+    );
+  };
 
   return (
     <>
-    {location === 'Home' && textHomePage()}
-    {location === 'Drink' && textDrinkPage()}
+      {location === 'Home' && textHomePage()}
+      {location === 'Drink' && textDrinkPage()}
     </>
   );
 }
